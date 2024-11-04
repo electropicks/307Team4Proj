10ef30a2546c2ee37201c80e96b1cd14b73dc60c
electropicks <lkbidy@gmail.com>
2024-11-04 09:23:42 -0800

    Adjust env variables to not expose to public browser information

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -6,8 +6,8 @@ export async function createClient() {
 
   console.log('creating server client');
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
