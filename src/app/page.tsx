093d5e90e1c3dd63e0fc27bcbbd7afd608a8881a
Liam <42216587+electropicks@users.noreply.github.com>
2024-11-13 16:26:07 -0800

    Tanstack and books api listing (#14)
    * Add tanstack query
    
    * Implement Books fetching api and table display/search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,87 @@
 'use client';
 
+import { useBooks } from '@/app/api/books';
+import { useState } from 'react';
+import { Search } from 'lucide-react';
+import dayjs from 'dayjs';
+
 export default function Home() {
+  const [searchInput, setSearchInput] = useState('');
+  const [bookSearch, setBookSearch] = useState('');
+  const { data: books, isLoading: isBooksLoading } = useBooks(bookSearch);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevents the default form submission behavior (page reload)
+    if (searchInput.trim() === '') {
+      return;
+    }
+    setBookSearch(searchInput);
+  };
+
   return (
     <div className="p-4 min-h-screen flex flex-col bg-background text-foreground items-center">
       <h1 className="bg-primary p-4 rounded-xl">Bookshelf</h1>
+      <div className="w-full max-w-sm min-w-[200px]">
+        <form onSubmit={handleFormSubmit} className="flex p-4">
+          <input
+            className="bg-secondary w-full text-foreground rounded-2xl p-2 focus:outline-neutral-none placeholder:text-accent"
+            placeholder="Search for books..."
+            value={searchInput}
+            onChange={handleInputChange}
+          />
+          <button
+            type="submit"
+            className="ml-2 p-2 bg-primary text-foreground rounded-2xl"
+          >
+            <Search />
+          </button>
+        </form>
+      </div>
+      <div className="w-full mt-4 flex justify-center">
+        {isBooksLoading ? (
+          <p>Loading...</p>
+        ) : books && books.length > 0 ? (
+          <table className="w-full max-w-2xl mt-4 table-auto border-collapse border border-accent text-center">
+            <thead>
+              <tr className="bg-secondary text-foreground">
+                <th className="border border-accent p-2">Thumbnail</th>
+                <th className="border border-accent p-2">Title</th>
+                <th className="border border-accent p-2">Publish Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {books.map((book) => (
+                <tr key={book.id} className="border-t border-accent">
+                  <td className="border border-accent p-2">
+                    {book.volumeInfo.imageLinks.thumbnail && (
+                      <img
+                        src={book.volumeInfo.imageLinks?.thumbnail}
+                        alt={`${book.volumeInfo.title} Thumbnail`}
+                        className="max-w-full h-auto mx-auto"
+                        style={{ maxHeight: '200px', width: 'auto' }}
+                      />
+                    )}
+                  </td>
+                  <td className="border border-accent p-2">
+                    {book.volumeInfo.title}
+                  </td>
+                  <td className="border border-accent p-2 no-wrap">
+                    {dayjs(book.volumeInfo.publishedDate).format(
+                      'MMMM D, YYYY',
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
     </div>
   );
 }
