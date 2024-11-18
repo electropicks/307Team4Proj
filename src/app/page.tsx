944916c2d6037983ba9b0e9aee22f7864f6c3950
nidhiraviprasad <nidhi.raviprasad@gmail.com>
2024-11-18 10:09:58 -0800

    fixed formatting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,39 +8,16 @@ import dayjs from 'dayjs';
 import Link from 'next/link';
 
 
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-const products = [
+const books1 = [
   {
     id: 1,
-    name: 'Basic Tee',
-    href: '#',
     imageSrc: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9780861542109/iron-widow-9780861542109_hr.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
   },
-  // More products...
   {
     id: 2,
-    name: 'Basic Tee',
-    href: '#',
     imageSrc: 'https://i.pinimg.com/originals/05/78/1a/05781a16b09694baa3aedf6aa9a9f9d7.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Mom',
   },
   {
     id: 3,
@@ -48,96 +25,55 @@ const products = [
     href: '#',
     imageSrc: 'https://i.pinimg.com/originals/4e/db/dd/4edbdd1bde987a0f10ce496b16332df6.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Grandma',
   },
   {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
+    id: 4,
     imageSrc: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9780861542109/iron-widow-9780861542109_hr.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
   },
-  // More products...
   {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
+    id: 5,
     imageSrc: 'https://i.pinimg.com/originals/05/78/1a/05781a16b09694baa3aedf6aa9a9f9d7.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Mom',
   },
   {
-    id: 3,
-    name: 'Basic Tee',
-    href: '#',
+    id: 6,
     imageSrc: 'https://i.pinimg.com/originals/4e/db/dd/4edbdd1bde987a0f10ce496b16332df6.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Grandma',
   },
   {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
+    id: 7,
     imageSrc: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9780861542109/iron-widow-9780861542109_hr.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
   },
-  // More products...
   {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
+    id: 8,
     imageSrc: 'https://i.pinimg.com/originals/05/78/1a/05781a16b09694baa3aedf6aa9a9f9d7.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Mom',
   },
   {
-    id: 3,
-    name: 'Basic Tee',
-    href: '#',
+    id: 9,
     imageSrc: 'https://i.pinimg.com/originals/4e/db/dd/4edbdd1bde987a0f10ce496b16332df6.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Grandma',
   },
 ]
 
 
-const products2 = [
+const books2 = [
   {
     id: 1,
-    name: 'Basic Tee',
-    href: '#',
     imageSrc: 'https://img.readthistwice.com/unsafe/696x1044/books/a339fddd-d5bd-4c41-a1d7-8a3fa8ced7a6.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
   },
-  // More products...
   {
     id: 2,
-    name: 'Basic Tee',
-    href: '#',
     imageSrc: 'https://i.pinimg.com/originals/05/78/1a/05781a16b09694baa3aedf6aa9a9f9d7.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Mom',
   },
   {
     id: 3,
-    name: 'Basic Tee',
-    href: '#',
     imageSrc: 'https://i.pinimg.com/originals/4e/db/dd/4edbdd1bde987a0f10ce496b16332df6.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Your Grandma',
   },
 ]
 
@@ -233,7 +169,7 @@ export default function Home() {
         </div>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-7 xl:gap-x-8">
-          {products.map((product) => (
+          {books1.map((product) => (
             <div key={product.id} className="group relative">
               <div className="aspect-h-0.5 aspect-w-0.5 w-half overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-56">
                 <img
@@ -264,7 +200,7 @@ export default function Home() {
         </div>
 
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-7 xl:gap-x-8">
-        {products2.map((product) => (
+        {books2.map((product) => (
           <div key={product.id} className="group relative">
             <div className="aspect-h-0.5 aspect-w-0.5 w-half overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-56">
               <img
