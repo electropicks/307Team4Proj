95c4fdf33db921c1bb24a7cc0085f1c8807d64cd
electropicks <lkbidy@gmail.com>
2024-12-06 09:27:38 -0800

    Increase queryClient stale time

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -7,7 +7,16 @@ import { system } from '@/app/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function RootLayout(props: { children: ReactNode }) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+        staleTime: 1000 * 60 * 6,
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider value={system}>
